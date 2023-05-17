7e7f141b82edd9a9ba262e09f2aa9f6d331c1126
Joao Vitor <joao@msi-gf63.local>
2023-05-16 21:50:31 -0300

    first commit

---
+++ components/Gift/index.tsx
@@ -0,0 +1,10 @@
+export default function Gift() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="w-24 h-6 bg-green-400"></div>
+      <div className="w-[85px] h-14 bg-green-600"></div>
+      <div className="absolute w-4 h-20 bg-red-500"></div>
+      <div className="absolute w-[85px] h-4 bg-red-500 top-10"></div>
+    </div>
+  );
+}
