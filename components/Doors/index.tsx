81bdb1452bbd59155a46f8f32eee00141d622aeb
Joao Vitor <joao@msi-gf63.local>
2023-05-20 17:20:27 -0300

    customizing door

--- components/Doors/index.tsx
+++ components/Doors/index.tsx
@@ -1,7 +1,24 @@
-export default function Doors() {
+export default function Doors({ selectedDoor }: any) {
+  const selectedPort = selectedDoor ? selectedDoor : "";
   return (
-    <div className="flex flex-col w-52 h-80 items-center p-4 bg-amber-800">
-      <div className="text-5xl"></div>
+    <div className="w-52 h-80 flex flex-col items-center mx-2 relative">
+      <div
+        className={`flex w-11/12 grow border-solid border-4 border-amber-900 ${
+          selectedPort ? `border-solid border-4 border-yellow-300` : ""
+        }`}
+      >
+        <div className="flex flex-col grow items-center p-4 bg-amber-800">
+          <div className={`text-5xl ${selectedPort ? `text-yellow-300` : ""}`}>
+            3
+          </div>
+          <div
+            className={`absolute self-start h-5 w-5 rounded-3xl bg-amber-900 top-[45%] ${
+              selectedPort ? `bg-yellow-300` : ""
+            }`}
+          ></div>
+        </div>
+      </div>
+      <div className="h-3 w-full bg-gray-300"></div>
     </div>
   );
 }
